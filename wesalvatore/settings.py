637a2e2783f404401717699837e4eae8f735ff21
Wamique Ansari <wamique00786@gmail.com>
2024-12-27 23:05:40 +0530

    Update settings.py

--- wesalvatore/settings.py
+++ wesalvatore/settings.py
@@ -92,8 +92,16 @@ WSGI_APPLICATION = "wesalvatore.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.mysql',
+        'NAME': 'wesalvatare',
+        'USER': 'root',
+        'PASSWORD': 'Asad2723',
+        'HOST': 'mysql',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
