4b91a14655f801c52172d3f868cfce486eab7835
Mohammad Asad <mohdasad.9506@gmail.com>
2024-12-29 00:34:11 +0530

    User & Volunteer Dashboard almost fixed

--- rescue/csp_middleware.py
+++ rescue/csp_middleware.py
@@ -7,7 +7,7 @@ class ContentSecurityPolicyMiddleware:
         response['Content-Security-Policy'] = (
             "default-src 'self'; "
             "script-src 'self' https://unpkg.com https://cdn.jsdelivr.net 'unsafe-inline' 'unsafe-eval'; "  # Allow scripts from trusted sources
-            "style-src 'self' https://unpkg.com https://cdn.jsdelivr.net 'unsafe-inline'; "  # Allow styles from trusted sources
+            "style-src 'self' https://unpkg.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com 'unsafe-inline'; "  # Allow styles from trusted sources
             "img-src 'self' data: https://*.tile.openstreetmap.org https://unpkg.com; "  # Allow images from trusted sources
             "font-src 'self' https://cdnjs.cloudflare.com; "  # Allow fonts from trusted sources
             "object-src 'none';"  # Disallow all object sources
