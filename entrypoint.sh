3f2f0454b4e7e0a91f96c790184c1461d8918141
pavan s <117181088+pavan-3000@users.noreply.github.com>
2025-02-20 19:04:10 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,19 @@
-#!/bin/sh
-
-echo "Waiting for PostgreSQL to be ready..."
-while ! nc -z $DATABASE_HOST 5432; do
+echo "Waiting for database to be ready..."
+while ! nc -z postgres_db  5432; do
   sleep 1
 done
 echo "Database is ready!"
 
-# Apply database migrations
 echo "Applying database migrations..."
-python manage.py migrate
+python manage.py makemigrations && python manage.py makemigrations accounts && python manage.py migrate
 
-# Apply migrations for specific apps
-python manage.py migrate accounts --fake
-python manage.py migrate rescue --fake
-python manage.py migrate adoption --fake
-python manage.py migrate donation --fake
-python manage.py migrate subscription --fake
-
-# Check if migrations were successful
 if [ $? -ne 0 ]; then
   echo "Migrations failed. Exiting..."
   exit 1
 fi
 
-# Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --verbosity 3
+python manage.py collectstatic --noinput
 
-# Start Gunicorn server
 echo "Starting Gunicorn server..."
-exec gunicorn wesalvatore.wsgi:application --bind 0.0.0.0:8000 --workers 4 --timeout 120 --access-logfile -
+exec gunicorn wesalvatore.wsgi:application --bind 0.0.0.0:8000 --workers 4
