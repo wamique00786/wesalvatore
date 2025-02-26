c1367676e96f2a540720955590e0d590bc17dbfd
pavan s <117181088+pavan-3000@users.noreply.github.com>
2025-02-26 15:48:37 +0530

    Update settings.py

--- wesalvator/settings.py
+++ wesalvator/settings.py
@@ -151,6 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
