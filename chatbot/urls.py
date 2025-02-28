3a0cfdf10e0e260709eb5ba550a15719112f2c40
Mohammad Asad <141928886+mohdasad05@users.noreply.github.com>
2025-02-28 18:04:45 +0530

    Update urls.py
    Fix chatbot bug

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chat_home, name='chat_home'),
+    #path('', views.chat_home, name='chat_home'),
     path('get_response/', views.get_bot_response, name='get_bot_response'),
     path('api/chatbot/', views.ChatbotAPIView.as_view(), name='chatbot_api'),
 
